9a969c1f85d4a90486c57734e382d29dd6f5b0c9
Shawn Davis <shawnandrewdavis@gmail.com>
2019-01-08 10:47:22 -0500

    first commit

---
+++ 01-grammarType/1-comments.js
@@ -0,0 +1,12 @@
+// A comment! 
+//what comments do for :
+//1) communicate with other delopers by writing in plain english
+//2) annotate certain sections of our code for our future selves
+
+/*
+I can 
+comment out
+multiple lines!
+*/
+console.log('yay');
+console.log('shawn');
